f7d8f80d278efa930048d8fdec00bfc857796ad1
romeroalan26 <alan.266@hotmail.com>
2023-01-15 22:26:30 -0400

    update

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment.js
@@ -1,14 +1,20 @@
+
 // LECTURE: Values and Variables
 
-let country = "Dominican Republic";
-let continent = "American Continent";
+const country = "Dominican Republic";
+const continent = "American Continent";
 let population = 11200000;
 console.log(country, continent, population);
 
 // LECTURE: Data Types
 
-let isIsland = true;
+const isIsland = true;
 let language;
 console.log(typeof isIsland, typeof population,
     typeof country, typeof language);
 
+
+
+// LECTURE: let, const and var
+
+language = "spanish";
--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -24,7 +24,7 @@ let job1 = "programmer";
 let hob2 = "teacher";
 
 console.log(myFirstJob);
-*/
+
 
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
@@ -45,3 +45,43 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Romero";
+console.log(lastName);
+*/
+
+//Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+//  2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedmann';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10;  // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--; // x = x - 1
+console.log(x);
+
+
+
