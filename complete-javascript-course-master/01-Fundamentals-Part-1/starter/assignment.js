4d79cc40b12833911c273a9ba9824b5d19a85dd5
romeroalan26 <alan.266@hotmail.com>
2023-01-15 20:01:16 -0400

    update

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,14 @@
+// LECTURE: Values and Variables
+
+let country = "Dominican Republic";
+let continent = "American Continent";
+let population = 11200000;
+console.log(country, continent, population);
+
+// LECTURE: Data Types
+
+let isIsland = true;
+let language;
+console.log(typeof isIsland, typeof population,
+    typeof country, typeof language);
+
