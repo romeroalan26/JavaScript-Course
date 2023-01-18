3ae649a9fbf0ae2c7b8df7e33256c4bd587cb81c
romeroalan26 <alan.266@hotmail.com>
2023-01-18 03:13:16 -0400

    update

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -171,4 +171,50 @@ let n = `1` + 1;
 n = n - 1;
 console.log(n);
 
+
+
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if (money) {
+  console.log(`Don't spend it all :)`);
+} else {
+  console.log(`You should get a job!`);
+}
+
+let height = 10;
+if (height) {
+  console.log(`YAY! Height is defined`);
+} else {
+  console.log(`Height is UNDEFINED`);
+}
+
+
+
+// Equiality operators == vs ===
+const age = 18;
+if (age === 18) console.log(`You just became an adult :D(strict)`);
+
+if (age == 18) console.log(`You just became an adult :D (loose)`);
+
+const favourite = Number(prompt(`What's your favorite number?`));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+  console.log(`Cool! 23 is an amazing number!`);
+} else if (favourite === 7) {
+  console.log(`7 is also a cool number`);
+} else if (favourite === 9) {
+  console.log(`9 is also a cool number`);
+} else {
+  console.log(`Number is not 23 or 7`);
+}
+
+if (favourite !== 23) console.log(`Why not 23?`);
 */
