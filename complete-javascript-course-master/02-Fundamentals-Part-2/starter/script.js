111609c7076f280fd9a00f3402def117a8dcc452
romeroalan26 <alan.266@hotmail.com>
2023-01-22 16:44:09 -0400

    update

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -13,7 +13,7 @@ if (hasDriversLicense) console.log(" I can drive :D");
 //cont interface = `Audio`;
 // const private = 534;
 // const if = 534;
-*/
+
 
 // Functions
 function logger() {
@@ -26,10 +26,14 @@ logger();
 logger();
 
 function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
   const juice = `Juice with ${apples} apples and ${oranges} oranges`;
   return juice;
 }
 
 const appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+*/
