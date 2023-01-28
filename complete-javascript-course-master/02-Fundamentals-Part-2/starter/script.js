67d11bc63da302d73c77a2d017ef07f204c6d51f
romeroalan26 <alan.266@hotmail.com>
2023-01-28 14:25:50 -0400

    update

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -341,3 +341,16 @@ for (let i = 0; i < jonas.length; i++) {
 }
 
 */
+
+// 48. Looping Backwards and looping in loops
+const jonas = [
+  `Jonas`,
+  `Schmedtmann`,
+  2037 - 1991,
+  `teacher`,
+  [`Michael`, `Peter`, `Steven`],
+];
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
