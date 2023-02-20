641a109cd3f46789ec2a6845be6d75bec1e7d00e
romeroalan26 <alan.266@hotmail.com>
2023-02-20 02:09:03 -0400

    update

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,37 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function () {
+    openModal();
+  });
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', function () {
+  closeModal();
+});
+
+overlay.addEventListener('click', function () {
+  closeModal();
+});
+
+document.addEventListener('keydown', function (e) {
+  let pressedKey = e.key;
+  if (pressedKey === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
